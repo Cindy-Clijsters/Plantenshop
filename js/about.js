68cf6751934319a6438e39098c1f921849f0cf27
Cindy Clijsters <cindy.clijsters@vdabgenk.be>
2019-05-09 15:55:54 +0200

    Added menu to about page

--- js/about.js
+++ js/about.js
@@ -47,8 +47,17 @@ $(function() { // $() is hetzelfde als  $(document).ready()
    container.append(keuzelijst).prepend(diefRechts);
    $('#team').after(container);
    
-    
+   // Maak de inhoudsopgave
+   var root = $('article')[0];
+   var list = $('<ol>');
+   
+   $('#toc').empty().append(walkTree(root, list, enterNode, exitNode));
+   
 });
+
+var arrKoppen   = ["h1", "h2", "h3", "h4", "h5", "h6"];
+var arrSections = ["article", "section", "aside", "nav"];
+var getal       = 1;
 
 var walkTree = function (root, $list, enter, exit) 
 {
@@ -71,12 +80,94 @@ var walkTree = function (root, $list, enter, exit)
                 continue start;
             }
             
-            if (node == root)
+            if (node === root) {
                 node = null;
-            else
+            } else {
                 node = node.parentNode;
+            }
         }
     }
     
+    console.log($list);
     return $list;
+}
+
+var checkNode = function(node) {
+    // controleert of deze node in aanmerking komt voor de inhoudsopgave
+    var strNotoc = "no-toc";
+    
+    return (node.nodeType == 1 && arrSections.indexOf(node.tagName.toLowerCase()) >= 0 && node.className.indexOf(strNotoc) == -1);
+}
+
+function enterNode(node, list) {
+    
+    //console.log('ENTER NODE');
+    
+    //console.log(node);
+    //console.log(list);
+    
+    // bouwt list op ij het binnengaan van een node
+    if (checkNode(node)) {
+        var nieuw = $('<li>').attr("tabindex", getal.toString());
+        var a     = $('<a>').attr({
+            "href" : "#" + getal.toString(),
+            "id"   : "o" + getal.toString()
+        });
+        
+        node.setAttribute("id", getal.toString());
+        getal++;
+        
+        a.text(zoekKoppen(node));
+        nieuw.append(a);      
+        
+        if (list[0].tagName === "LI") {
+            //console.log(1);
+            var nieuweLijst = $('<ol>').append(nieuw);
+            list.append(nieuweLijst);
+            list = nieuw;
+        } else {
+            //console.log(2);
+            list.append(nieuw);
+            list = nieuw;
+        }
+        
+        //console.log(list);
+        
+        
+    }
+    
+    return list;
+}
+
+var exitNode = function(node, list) {
+    
+    // bij het verlaten van de node
+    if (checkNode(node)) {
+        if(list[0].tagName = "OL") {
+            list = list.parent();
+        }
+        
+        list = list.parent();
+    }
+    
+    return list;
+}
+
+var zoekKoppen = function(node) {
+    
+    var node     = $(node);
+    var koptekst = "";
+    
+    
+    // Zoek de hoogste kop, return zijn tekst
+    $.each(arrKoppen, function(i, v) {
+        var kop = $(v, node);
+        
+        if (kop.length > 0) {
+            koptekst = kop.first().text();
+            return false;
+        }  
+    })
+    
+    return koptekst;
 }
