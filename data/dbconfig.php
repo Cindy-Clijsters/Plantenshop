d3609c71fd4686d30224b68005120d4b83bf9de6
Cindy Clijsters <cindy.clijsters@gmail.com>
2019-05-14 11:29:43 +0200

    Removed creating index list

--- data/dbconfig.php
+++ data/dbconfig.php
@@ -1,15 +1,10 @@
 <?php
-
-
 class DBConfig {
 
-	//geen instance van deze class wordt aangemaakt, dient enkel als container
-	public static $host = "localhost";
-	public static $dbUser = "plantenshop";
-	public static $dbPassw = "pYfyp7IfBEDBZyCF";
-	public static $dbName = "plantenshop";
-
+    //geen instance van deze class wordt aangemaakt, dient enkel als container
+    public static $host    = "localhost";
+    public static $dbUser  = "plantenshop";
+    public static $dbPassw = "pYfyp7IfBEDBZyCF";
+    public static $dbName  = "plantenshop";
 
 }
-
-?>
--- js/about.js
+++ js/about.js
@@ -13,11 +13,13 @@ $(function() { // $() is hetzelfde als  $(document).ready()
     }); 
     
     // Add the Back to top buttons
+    /*
     $('<button class="toTop ui-button ui-widget ui-corner-all">Terug naar boven</button>').insertBefore(':header:gt(1)');
     
     $('button.toTop').on('click', function(){
         $(document).scrollTop( $("#about").offset().top );
     }); 
+    */
     
     // Add team members
     var lijst = ['Roger', 'Evelyn', 'Hilde', 'Jan'];
@@ -48,10 +50,10 @@ $(function() { // $() is hetzelfde als  $(document).ready()
    $('#team').after(container);
    
    // Maak de inhoudsopgave
-   var root = $('article')[0];
-   var list = $('<ol>');
+   var root  = $('article')[0];
+   var $list = $('<ol>');
    
-   $('#toc').empty().append(walkTree(root, list, enterNode, exitNode));
+   //$('#toc').empty().append(walkTree(root, $list, enterNode, exitNode));
    
 });
 
@@ -62,11 +64,15 @@ var getal       = 1;
 var walkTree = function (root, $list, enter, exit) 
 {
     var node = root;
-
+    
+    //console.log(root);
+    
     start: while (node) {
+        
+        //console.log(node);
 
         $list = enter(node,$list);
-
+        
         if (node.firstChild) {
             node = node.firstChild;
             continue start;
@@ -86,88 +92,89 @@ var walkTree = function (root, $list, enter, exit)
                 node = node.parentNode;
             }
         }
+        
     }
     
-    console.log($list);
     return $list;
 }
 
 var checkNode = function(node) {
+    
     // controleert of deze node in aanmerking komt voor de inhoudsopgave
+    // enkel als elementNode, in de lijst sectionsElms en geen no-toc
     var strNotoc = "no-toc";
     
-    return (node.nodeType == 1 && arrSections.indexOf(node.tagName.toLowerCase()) >= 0 && node.className.indexOf(strNotoc) == -1);
+    return (node.nodeType === 1 && arrSections.indexOf(node.tagName.toLowerCase()) >= 0 && node.className.indexOf(strNotoc) === -1);
 }
 
-function enterNode(node, list) {
+function enterNode(node, $list) {
     
-    //console.log('ENTER NODE');
-    
-    //console.log(node);
-    //console.log(list);
-    
-    // bouwt list op ij het binnengaan van een node
+    //bouwt $list op bij het binnengaan van een node
     if (checkNode(node)) {
-        var nieuw = $('<li>').attr("tabindex", getal.toString());
-        var a     = $('<a>').attr({
-            "href" : "#" + getal.toString(),
-            "id"   : "o" + getal.toString()
-        });
+        
+        var $nieuw = $('<li>').attr("tabindex", getal.toString());
+        var $a     = $('<a>').attr({"href" : "#" + getal.toString(), "id" : "o" + getal.toString() });
         
         node.setAttribute("id", getal.toString());
         getal++;
+        $a.text(zoekKoppen(node));
+        $nieuw.append($a);
         
-        a.text(zoekKoppen(node));
-        nieuw.append(a);      
+        //console.log("TEST: " + $list[0]);
         
-        if (list[0].tagName === "LI") {
-            //console.log(1);
-            var nieuweLijst = $('<ol>').append(nieuw);
-            list.append(nieuweLijst);
-            list = nieuw;
-        } else {
-            //console.log(2);
-            list.append(nieuw);
-            list = nieuw;
-        }
+        //if (typeof $list[0] !== 'undefined'){
+            if ($list[0].tagName === "LI") {
+                var $nieuweLijst = $('<ol>').append($nieuw); $list.append($nieuweLijst);
+                $list =$nieuw;
+            } else {
+                $list =$nieuw;
+            }
+        //} else {
+        //   $list =$nieuw;
+        //}
         
-        //console.log(list);
-        
+        $list.append($nieuw);
         
     }
     
-    return list;
+    return $list;
 }
 
-var exitNode = function(node, list) {
+var exitNode = function(node,$list) {
     
-    // bij het verlaten van de node
+    //bij het verlaten van de node
     if (checkNode(node)) {
-        if(list[0].tagName = "OL") {
-            list = list.parent();
-        }
         
-        list = list.parent();
+        //if (typeof $list[0] !== 'undefined'){
+            
+        
+            if ($list[0].tagName === "OL") {
+                $list = $list.parent();
+            }
+            
+            $list = $list.parent();
+        
+        //}
+        
+        
     }
     
-    return list;
+    return $list;
 }
 
-var zoekKoppen = function(node) {
-    
-    var node     = $(node);
+var zoekKoppen = function(node)
+{
+    var $node = $(node);
     var koptekst = "";
-    
-    
-    // Zoek de hoogste kop, return zijn tekst
-    $.each(arrKoppen, function(i, v) {
-        var kop = $(v, node);
-        
-        if (kop.length > 0) {
-            koptekst = kop.first().text();
+
+    //zoek de hoogste kop, return zijn tekst
+    $.each(arrKoppen,function(i,v) {
+        var $kop = $(v,$node);
+        if ($kop.length > 0) {
+            koptekst = $kop.first().text();
             return false;
-        }  
-    })
+        }
+    });
     
     return koptekst;
 }
